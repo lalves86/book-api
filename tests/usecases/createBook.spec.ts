93b25a183a634e7bee3870da10156b87b4e15bc2
lalves86 <lucas.alves@engenharia.ufjf.br>
2022-02-04 17:36:03 -0300

    refactor: move sut creation to a factory helper method

--- tests/usecases/createBook.spec.ts
+++ tests/usecases/createBook.spec.ts
@@ -1,8 +1,27 @@
 import { Book } from '@/domain/book'
 import { CreateBook } from '@/usecases/createBook'
+class BookRepositoryStub {
+  async create (book: Book): Promise<Book> {
+    return Promise.resolve({
+      title: 'Fake Title',
+      author: 'Fake Author',
+      createdAt: new Date(),
+      finishedAt: new Date(),
+      grade: 5,
+      status: 'Read'
+    })
+  }
+}
+
+const makeSut = (): CreateBook => {
+  const bookRepositoryStub = new BookRepositoryStub()
+  return new CreateBook(bookRepositoryStub)
+}
 
 describe('CreateBook', () => {
   it('should call book repository with correct parameters', async () => {
+    const createBookUsecase = makeSut()
+
     const fakeBook: Book = {
       title: 'Fake Title',
       author: 'Fake Author',
@@ -11,14 +30,6 @@ describe('CreateBook', () => {
       grade: 5,
       status: 'Read'
     }
-
-    class BookRepositoryStub {
-      async create (book: Book): Promise<Book> {
-        return Promise.resolve(fakeBook)
-      }
-    }
-
-    const createBookUsecase = new CreateBook(new BookRepositoryStub())
     const result = await createBookUsecase.execute(fakeBook)
 
     expect(result).toEqual(fakeBook)

03ee5e4f39cb5dfcd7889b0d58e99f19a7b07b4a
lalves86 <lucas.alves@engenharia.ufjf.br>
2022-02-18 14:28:24 -0300

    refactor: change interface parameter types to use dtos

--- src/infra/repositories/ports/bookRepository.ts
+++ src/infra/repositories/ports/bookRepository.ts
@@ -1,10 +1,12 @@
 import { Book } from '@/domain/book'
+import { CreateBookDto } from '@/usecases/dtos/createBookDto'
+import { UpdateBookDto } from '@/usecases/dtos/updateBookDto'
 
 export interface BookRepository {
-  create (book: Book): Promise<Book>
+  create (book: CreateBookDto): Promise<Book>
   list (): Promise<Book[]>
   listById (id: string): Promise<Book>
   getByTitle (title: string): Promise<Book>
-  update (book: Book): Promise<Book>
+  update (book: UpdateBookDto): Promise<Book>
   delete (id: string): Promise<string>
 }

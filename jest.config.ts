45adc35b89d0be4adb78a4905602abf5686553d5
lalves86 <lucas.alves@engenharia.ufjf.br>
2022-02-25 14:13:57 -0300

    chore: change jest config to exclude files not tested

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,13 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!**/tests/**',
-    '!**/config/**'
+    '!**/config/**',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/infra/**',
+    '!<rootDir>/src/app.ts',
+    '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/**/ports/**',
+    '!<rootDir>/src/**/dtos/**'
   ],
   testEnvironment: 'node',
   transform: {

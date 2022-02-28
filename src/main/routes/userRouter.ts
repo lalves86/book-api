d2862dcd996fd205a81f702495d76474fa54827c
lalves86 <lucas.alves@engenharia.ufjf.br>
2022-02-28 20:07:33 -0300

    feat: make listUserById a private route

--- src/main/routes/userRouter.ts
+++ src/main/routes/userRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/userExpressRouteAdapter'
 import { makeUserController } from '../factories/makeUserController'
+import { ensureAuthenticated } from '../middlewares/authMiddleware'
 
 const userRouter = Router()
 
 const userController = adaptRoute(makeUserController())
 
 userRouter.post('/', userController.create)
+userRouter.use(ensureAuthenticated)
 userRouter.get('/:id', userController.show)
 
 export default userRouter
